52eeca250ceb45174785097960e033ef50d3f47a
Marciano Valverde <marcianovc@gmail.com>
2016-09-13 14:34:54 -0300

    2.5.4.2

--- src/version.h
+++ src/version.h
@@ -30,10 +30,10 @@ static const int DATABASE_VERSION = 70508;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 62016;
+static const int PROTOCOL_VERSION = 620161;
 
 // earlier versions not supported as of Abr 2016, and are disconnected
-static const int MIN_PROTO_VERSION = 62016;
+static const int MIN_PROTO_VERSION = 620161;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
@@ -44,9 +44,9 @@ static const int NOBLKS_VERSION_START = 60002;
 static const int NOBLKS_VERSION_END = 60006;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 62016;
+static const int BIP0031_VERSION = 620161;
 
 // "mempool" command, enhanced "getdata" behavior starts with this version:
-static const int MEMPOOL_GD_VERSION = 62016;
+static const int MEMPOOL_GD_VERSION = 620161;
 
 #endif
