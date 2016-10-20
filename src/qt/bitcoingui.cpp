85b56ee780fa1bc67c9ea5ebfdbf595fbd70170a
Marciano Valverde <marcianovc@gmail.com>
2016-10-20 11:15:40 -0200

    fix minimizing qt app

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1037,18 +1037,26 @@ void BitcoinGUI::showNormalIfMinimized(bool fToggleHidden)
     // activateWindow() (sometimes) helps with keyboard focus on Windows
     if (isHidden())
     {
+        // Make sure the window is not minimized
+        setWindowState(windowState() & (~Qt::WindowMinimized | Qt::WindowActive));
+        // Then show it
         show();
+	raise();
         activateWindow();
     }
     else if (isMinimized())
     {
         showNormal();
+	raise();
         activateWindow();
     }
     else if (GUIUtil::isObscured(this))
     {
         raise();
         activateWindow();
+        sleep(1);
+        if (GUIUtil::isObscured(this))
+            hide();
     }
     else if(fToggleHidden)
         hide();
