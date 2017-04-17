9646b58ea2b8930c20ab77b5634b921599acbf97
Marciano Valverde <marcianovc@gmail.com>
2017-04-17 17:26:26 -0300

    fix

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -201,6 +201,11 @@ void TransactionView::clearOrphans()
         return;
 
     model->clearOrphans();
+    model->getTransactionTableModel()->refresh();
+    delete transactionProxyModel;
+    setModel(model);
+    transactionView->sortByColumn(TransactionTableModel::Status, Qt::DescendingOrder);
+    transactionView->sortByColumn(TransactionTableModel::Date, Qt::DescendingOrder);
 }
 
 void TransactionView::chooseDate(int idx)
