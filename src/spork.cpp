ff60810e791b5e5016805148d5893cd72b170934
Marciano Valverde <marcianovc@gmail.com>
2020-09-17 11:33:56 -0300

    spork8 disabled

--- src/spork.cpp
+++ src/spork.cpp
@@ -21,11 +21,12 @@ std::map<int, int64_t> mapSporkDefaults = {
     {SPORK_3_FASTSEND_BLOCK_FILTERING,    0},             // ON
     {SPORK_5_FASTSEND_MAX_VALUE,          1000},          // 1000 MarteX
     {SPORK_6_NEW_SIGS,                       4070908800ULL}, // OFF
-    {SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT, 0}, // ON
+    {SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT, 4070908800ULL}, // OFF
     {SPORK_9_SUPERBLOCKS_ENABLED,            0}, // ON
     {SPORK_10_MASTERNODE_PAY_UPDATED_NODES,  4070908800ULL}, // OFF
     {SPORK_12_RECONSIDER_BLOCKS,             0},             // 0 BLOCKS
     {SPORK_14_REQUIRE_SENTINEL_FLAG,         4070908800ULL}, // OFF
+    {SPORK_20_FORCE_ENABLED_MASTERNODE,      0}, // ON
 };
 
 void CSporkManager::ProcessSpork(CNode* pfrom, const std::string& strCommand, CDataStream& vRecv, CConnman& connman)
